b1fb8ff85d4eea6c0a653f41effd2884082f84cf
Lock <167706567+NotMageLock@users.noreply.github.com>
2024-04-29 18:23:21 -0400

    Update PluginInfo.cs

--- GravGrongle/PluginInfo.cs
+++ GravGrongle/PluginInfo.cs
@@ -4,6 +4,6 @@
 	{
 		public const string GUID = "com.magelock.gorillatag.gravgrongle";
 		public const string Name = "GravGrongle";
-		public const string Version = "1.0.0";
+		public const string Version = "1.1.0";
 	}
 }
